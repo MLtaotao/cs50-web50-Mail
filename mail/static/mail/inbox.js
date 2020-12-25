db719111c800da35db39bac67033b69acf6f2f33
MLtaotao <shuiwentao@gmail.com>
2020-12-25 14:40:58 +0800

    last version

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -19,6 +19,8 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#email-view').style.display = 'none';
+
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -34,7 +36,6 @@ function load_mailbox(mailbox) {
   document.querySelector('#email-view').style.display = 'none';
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
@@ -50,36 +51,83 @@ function load_mailbox(mailbox) {
     table.id = "tableId";
     //Creat thead
     let table_head = document.createElement('thead');
-    table_head.innerHTML = "<th scope=\"col\">Id</th><th scope=\"col\">Sender</th><th scope=\"col\">Subject</th><th scope=\"col\">Timestamp</th>";
+    table_head.innerHTML = "<th scope=\"col\">Id</th><th scope=\"col\">Sender</th><th scope=\"col\">Subject</th><th scope=\"col\">Timestamp</th><th scope=\"col\"></th>";
     
     table.append(table_head);
     // creat table body and add element
     let table_body = document.createElement('tbody');
-    emails.forEach(email => {
-      let table_row = document.createElement('tr');
-      table_row.id = email.id;
-      var datetime = new Date(email.timestamp)
-      table_row.innerHTML = `<th scope="row">${email.id}</th>
-      <td>${email.sender}</td>
-      <td>${email.subject}</td>
-      <td>${datetime.toLocaleString()}</td>`;
-
-      // set table row link to the email
-      table_row.addEventListener('click', () => view_email(email.id), false);
-
-      //If the email is unread, it should appear with a white background. If the email has been read, it should appear with a gray background.
-      if (email.read !== false) {
-        table_row.style.backgroundColor = 'lightgray'
-      }
-      table_body.append(table_row);
-
-
-    });
+    if (mailbox == "inbox") {
+      emails.forEach(email => {
+        let table_row = document.createElement('tr');
+        table_row.id = email.id;
+        var datetime = new Date(email.timestamp);
+        table_row.innerHTML = `<th scope="row">${email.id}</th>
+        <td>${email.sender}</td>
+        <td>${email.subject}</td>
+        <td>${datetime.toLocaleString()}</td>
+        <td><button type="button" class="btn btn-secondary id="mail-archive">Archive</button></td>`;
+  
+        // set table row link to the email
+        table_row.getElementsByTagName('td')[0].addEventListener('click', () => view_email(email.id), false);
+        table_row.getElementsByTagName('td')[1].addEventListener('click', () => view_email(email.id), false);
+        table_row.getElementsByTagName('td')[2].addEventListener('click', () => view_email(email.id), false);
+        table_row.getElementsByTagName('td')[3].addEventListener('click', () => archive_email(email.id), false);
+        // table_row.addEventListener('click', () => view_email(email.id), false);
+  
+        //If the email is unread, it should appear with a white background. If the email has been read, it should appear with a gray background.
+        if (email.read !== false) {
+          table_row.style.backgroundColor = 'lightgray'
+        }
+        table_body.append(table_row);
+      });
+    } 
+    else if (mailbox == "archive"){
+      emails.forEach(email => {
+        let table_row = document.createElement('tr');
+        table_row.id = email.id;
+        var datetime = new Date(email.timestamp)
+        table_row.innerHTML = `<th scope="row">${email.id}</th>
+        <td>${email.sender}</td>
+        <td>${email.subject}</td>
+        <td>${datetime.toLocaleString()}</td>
+        <td><button type="button" class="btn btn-secondary id="mail-archive">Unarchive</button></td>`;
+  
+        // set table row link to the email
+        table_row.getElementsByTagName('td')[0].addEventListener('click', () => view_email(email.id), false);
+        table_row.getElementsByTagName('td')[1].addEventListener('click', () => view_email(email.id), false);
+        table_row.getElementsByTagName('td')[2].addEventListener('click', () => view_email(email.id), false);
+        table_row.getElementsByTagName('td')[3].addEventListener('click', () => unarchive_email(email.id), false);
+        // table_row.addEventListener('click', () => view_email(email.id), false);
+  
+        //If the email is unread, it should appear with a white background. If the email has been read, it should appear with a gray background.
+        if (email.read !== false) {
+          table_row.style.backgroundColor = 'lightgray'
+        }
+        table_body.append(table_row);
+      });
+    }
+    else {
+      emails.forEach(email => {
+        let table_row = document.createElement('tr');
+        table_row.id = email.id;
+        var datetime = new Date(email.timestamp)
+        table_row.innerHTML = `<th scope="row">${email.id}</th>
+        <td>${email.sender}</td>
+        <td>${email.subject}</td>
+        <td>${datetime.toLocaleString()}</td>`;
+  
+        // set table row link to the email
+        table_row.getElementsByTagName('td')[0].addEventListener('click', () => view_email(email.id), false);
+        table_row.getElementsByTagName('td')[1].addEventListener('click', () => view_email(email.id), false);
+        table_row.getElementsByTagName('td')[2].addEventListener('click', () => view_email(email.id), false);
+        // table_row.addEventListener('click', () => view_email(email.id), false);
+  
+        table_body.append(table_row);
+      });
+    }
 
     table.append(table_body);
-
     table.style.textAlign = 'center';
-
     document.querySelector('#emails-view').append(table);
 
   });
@@ -120,7 +168,7 @@ function view_email(email_id) {
       document.querySelector('#email-subject').innerHTML = `<b class="font-weight-bolder"> Subject:</b> ${email.subject}`;
       document.querySelector('#email-timestamp').innerHTML = `<b class="font-weight-bolder"> Timestamp:</b> ${datetime.toLocaleString()}`;
       document.querySelector('#email-body').innerHTML = `<p class="text-monospace">${email.body}</p>`;
-
+      document.querySelector('#email-reply').addEventListener('click',() => reply_email(email), false)
   });
   //Once the email has been clicked on, you should mark the email as read. Recall that you can send a PUT request to /emails/<email_id> to update whether an email is read or not.
   fetch(`/emails/${email_id}`, {
@@ -138,4 +186,39 @@ function archive_email(email_id) {
         archived: true
     })
   });
+  location.reload();
+}
+
+function unarchive_email(email_id) {
+  fetch(`/emails/${email_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        archived: false
+    })
+  });
+  location.reload();
+}
+
+function reply_email(email) {
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#email-view').style.display = 'none';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = `${email.sender}`;
+  document.querySelector('#compose-recipients').disabled = true;
+  if (email.subject.substring(0,4)=='Re: ') {
+    document.querySelector('#compose-subject').value = email.subject;
+  }
+  else {
+    document.querySelector('#compose-subject').value = `Re: ${email.subject}`;
+  }
+  var datetime = new Date(email.timestamp);
+  document.querySelector('#compose-body').value = `
+  
+
+  ---------------
+  On ${datetime.toLocaleString()} ${email.sender} wrote:
+  ${email.body}`;
 }
